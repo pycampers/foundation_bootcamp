006ed4ac2c07a7359267b7045a45a9a48ac79f08
Hasan <hasanhack89@gmail.com>
2019-10-18 17:04:34 +0530

    added front end to the food donation

--- backend_dev/15_food_inventory/app.py
+++ backend_dev/15_food_inventory/app.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for, redirect
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
 
+
+"""
+TODO
+
+1. Accept capital city names
+2. Food type to drop down
+3. Make a home page with links to donate and find food page
+"""
 
 file_name = 'FoodDonation.db'
 
@@ -24,35 +32,51 @@ class FoodDonation(db.Model):
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/food_donation', methods=['POST'])
+@app.route('/food_donation', methods=['GET', 'POST'])
 def food_donation():
-    data_fields = ['name',
-                    'age',
-                    'expiry',
-                    'food_type',
-                    'city',
-                    'phone_no',
-                    'quantity']
-    data_dict = {}
-    for field in data_fields:
-        data_dict[field] = request.form.get(field)
-    food_donation = FoodDonation(**data_dict)
-    db.session.add(food_donation)
-    db.session.commit()
+    if request.method == 'GET':
+        return render_template('donate.html')
+    else:
+        data_fields = ['name',
+                        'age',
+                        'expiry',
+                        'food_type',
+                        'city',
+                        'phone_no',
+                        'quantity']
+        data_dict = {}
+        for field in data_fields:
+            data_dict[field] = request.form.get(field)
+        food_donation = FoodDonation(**data_dict)
+        db.session.add(food_donation)
+        db.session.commit()
 
-    return jsonify(data_dict)
+        return redirect(url_for('food_donation'))
 
-@app.route('/food_receive', methods=['POST'])
+@app.route('/food_receive', methods=['GET','POST'])
 def food_receive():
-    food_type = request.form.get('food_type')
-    city = request.form.get('city')
-    result = FoodDonation.query. \
-        filter_by(food_type=food_type). \
-        filter_by(city=city). \
-            all()
-            
+    if request.method == 'GET':
+        return render_template('find_food.html')
+    else:
+        food_type = request.form.get('food_type')
+        city = request.form.get('city')
+        result = FoodDonation.query. \
+            filter_by(food_type=food_type). \
+            filter_by(city=city). \
+                all()
+                
+        print(result)
+        return render_template('results.html', food_donations=result)
+
+@app.route('/delete_food')
+def delete_food():
+    food_donation_id = request.args.get('id')
+    result = FoodDonation.query.get(food_donation_id)
     print(result)
-    return f'{city} {food_type}'
+    db.session.delete(result)
+    db.session.commit()
+    return redirect(url_for('food_receive'))
+
 
 
 if __name__ == '__main__':
